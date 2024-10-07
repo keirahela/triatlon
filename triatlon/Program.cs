44d459bf1b75f2023a6d1e6db210031a5441162c
Kósa Márk Milán <kosa.mark@verebely.dc>
2024-10-07 10:05:37 +0200

    initial commit

---
+++ triatlon/Program.cs
@@ -0,0 +1,70 @@
+﻿using triatlon;
+
+List<Versenyzo> versenyzok = new List<Versenyzo>();
+
+StreamReader sr = new StreamReader("forras.txt");
+
+while(!sr.EndOfStream)
+{
+    string[] line = sr.ReadLine().Split(";");
+
+    Versenyzo versenyzo = new Versenyzo();
+
+    versenyzo.Name = line[0];
+    versenyzo.szuletesiev = int.Parse(line[1]);
+    versenyzo.rajtszam = int.Parse(line[2]);
+    versenyzo.nem = line[3];
+    versenyzo.evkategoria = line[4];
+    versenyzo.UszasIdeje = TimeSpan.ParseExact(line[5], "hh\\:mm\\:ss", null);
+    versenyzo.elsodepobantoltottido = TimeSpan.ParseExact(line[6], "hh\\:mm\\:ss", null);
+    versenyzo.kerekparozasideje = TimeSpan.ParseExact(line[7], "hh\\:mm\\:ss", null);
+    versenyzo.masodikdepobantoltottido = TimeSpan.ParseExact(line[8], "hh\\:mm\\:ss", null);
+    versenyzo.futasideje = TimeSpan.ParseExact(line[9], "hh\\:mm\\:ss", null);
+
+    versenyzok.Add(versenyzo);
+}
+
+sr.Close();
+
+Console.WriteLine($"{versenyzok.Count} versenyző fejezte be a versenyt");
+
+int elitversenyzok = versenyzok.Count(x => x.evkategoria == "elit");
+
+Console.WriteLine($"{elitversenyzok} db elit versenyző van.");
+
+double noiatlageletkor = versenyzok.Where(x => x.nem == "n").Average(x => DateTime.Now.Year - x.szuletesiev);
+
+Console.WriteLine($"A női átlag életkor {noiatlageletkor}");
+
+double kerekparozasosszido = versenyzok.Sum(x => x.kerekparozasideje.TotalHours);
+
+Console.WriteLine($"Kerékpározás összidő: {Math.Round(kerekparozasosszido, 2)} óra");
+
+double atlaguszaselit = versenyzok.Where(x => x.evkategoria == "elit junior").Average(x => x.UszasIdeje.TotalMinutes);
+
+Console.WriteLine($"Átlagos úszási idő elit junior kategóriában: {atlaguszaselit} perc");
+
+var ferfiGyoztes = versenyzok.Where(v => v.nem == "f").OrderBy(v => v.Osszido).FirstOrDefault();
+
+Console.WriteLine();
+Console.WriteLine(ferfiGyoztes.ToString());
+Console.WriteLine();
+
+var korkategoriak = versenyzok.GroupBy(v => v.evkategoria);
+foreach (var kategoria in korkategoriak)
+{
+    Console.WriteLine($"Korcsoport: {kategoria.Key} - Befejezők száma: {kategoria.Count()}");
+}
+Console.WriteLine();
+
+var atlagDepoIdoKorcsoportonkent = versenyzok.GroupBy(v => v.evkategoria)
+    .Select(g => new
+    {
+        Korosztaly = g.Key,
+        AtlagDepoIdo = g.Average(v => v.elsodepobantoltottido.TotalMinutes + v.masodikdepobantoltottido.TotalMinutes)
+    });
+
+foreach (var item in atlagDepoIdoKorcsoportonkent)
+{
+    Console.WriteLine($"Korcsoport: {item.Korosztaly} - Átlag depó idő: {Math.Round(item.AtlagDepoIdo, 2)} perc");
+}
---
+++ triatlon/Versenyzo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triatlon
+{
+    internal class Versenyzo
+    {
+        public string Name { get; set; }
+        public int szuletesiev { get; set; }
+        public int rajtszam { get; set; }
+        public string nem { get; set; }
+        public string evkategoria { get; set; }
+        public TimeSpan UszasIdeje { get; set; }
+        public TimeSpan elsodepobantoltottido { get; set; }
+        public TimeSpan kerekparozasideje { get; set; }
+        public TimeSpan masodikdepobantoltottido { get; set; }
+        public TimeSpan futasideje { get; set; }
+        public TimeSpan Osszido => UszasIdeje + elsodepobantoltottido + kerekparozasideje + masodikdepobantoltottido + futasideje;
+
+        public override string ToString()
+        {
+            return $"Név: {Name}\nSzületési év: {szuletesiev}\nRajtszám: {rajtszam}\nNem: {nem}\nKategória: {evkategoria}\n\nIdőeredmények:" +
+                $"\nÚszás ideje: {UszasIdeje}\nElső depóban töltött idő: {elsodepobantoltottido}\nKerékpározás ideje: {kerekparozasideje}" +
+                $"\nMásodik depóban töltött idő: {masodikdepobantoltottido}\nFutás ideje: {futasideje}";
+        }
+    }
+}
